e3b43f045b8e3b784293cf969ec7454e604f6817
jr-jader <juuniorcaardoso@gmail.com>
2024-08-21 21:15:32 -0300

    sintatico

---
+++ Prático/Lista_9/sintatico.h
@@ -0,0 +1,16 @@
+#ifndef _SINTATICO_H_
+#define _SINTATICO_H_
+
+#define END_OF_FILE -1
+
+#define IF 1
+#define THEN 2
+#define ELSE 3
+#define BEGIN 4
+#define END 5
+#define PRINT 6
+#define SEMI 7
+#define NUM 8
+#define EQ 9
+
+#endif
